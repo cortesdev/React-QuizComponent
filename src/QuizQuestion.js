0cb753629ea39480bfcfd11c00b621f74c2ce3e6
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2018-11-28 15:25:40 +0100

    Completed the module-3.7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,7 +9,7 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                 <ul>
-                    //quiz question button logic goes here
+                    <li>{this.props.quiz_question.answer_options[0]}</li>
                 </ul>
                 </section>
             </main>
