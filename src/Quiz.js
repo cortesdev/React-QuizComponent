08f0be02bc7eedfba2d89c45bd7ba4232ae9ae95
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2018-11-28 13:50:18 +0100

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
     }
     render () {
         return (
-            <div><div className="QUizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+            <div>Quiz</div>
         )
     }
 }
