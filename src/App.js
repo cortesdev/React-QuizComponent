05756a8b977b4c6cc95a7baa2f7a945d31cce756
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2018-11-28 15:42:25 +0100

    Completed the module-3.9

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import Quiz from './Quiz.js'
 import './App.css'
-import QuizQuestion from './QuizQuestion';
-
+ 
 class App extends Component {
   render() {
     return (
